69d7b5fd76db550cae076a2110a70e384651c57e
Minjae Kim <haedaal@gmail.com>
2018-02-21 12:25:50 +0900

    feat(package): trigger setup.js on install

---
+++ src/setup.js
@@ -0,0 +1,19 @@
+const { execSync } = require('child_process')
+
+packages = [
+  { name: 'commitizen' /* default value for key 'save': '--save-dev' */ },
+  { name: 'cz-conventional-changelog' },
+  { name: 'eslint' },
+  { name: 'eslint-config-prettier' },
+  { name: 'husky' },
+  { name: 'lint-staged' },
+  { name: 'prettier' },
+  { name: 'semantic-release' },
+  { name: 'validate-commit-msg' },
+]
+
+function installPackages(packages) {
+  packages.forEach(package => {
+    execSync(`npm install ${package.name} ${package.save ? package.save : '--save-dev'}`)
+  })
+}

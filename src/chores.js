cae05f88682602c8d682e52420ac26a0cdc47076
Minjae Kim <haedaal@gmail.com>
2018-02-26 16:12:56 +0900

    feat: remove self

--- src/chores.js
+++ src/chores.js
@@ -1,7 +1,16 @@
+/**
+ * CAUTION
+ * This file will be copied to & run in root directory of containing project
+ */
+
 const { execSync } = require('child_process')
+const { unlinkSync } = require('fs')
 const path = require('path')
 
 execSync(`node node_modules/chores/src/setup.js`)
 
 console.info('removing node_modules/chores')
 execSync(`rm -rf node_modules/chores`)
+
+// remove self
+unlinkSync('./chores.js')
